440c4674bcdfa0414afcc7aa428f1d2eb90848f2
Bevis <Bevis.Goh@Gmail.com>
2019-04-16 00:58:40 +0800

    optimize code

--- src/BreatheTime.java
+++ src/BreatheTime.java
@@ -1,9 +1,8 @@
-import java.awt.*;
-
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
 public class BreatheTime extends JFrame {
